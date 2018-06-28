3300fe98bbdda54fd619fb32563fd9074e6836c8
IvanProdaiko94 <prodaiko94@gmail.com>
2018-06-28 16:33:28 +0300

    fixes

--- go/wg.go
+++ go/wg.go
@@ -12,7 +12,7 @@ func main() {
 		wg.Add(1)
 		go func(i int) {
 			defer wg.Done()
-			<-time.After(time.Duration(time.Second * time.Duration(i)))
+			<-time.After(time.Second)
 			fmt.Printf("Goroutine number %d finished\n", i)
 		}(i)
 	}
